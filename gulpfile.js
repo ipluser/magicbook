8aecc743dda8bf32c4cb637930f6f6eb11994a45
ipluser <ipluser@163.com>
2016-05-05 22:52:07 +0800

    refactor: add gulp task and lint

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,55 @@
-const gulp = require('gulp');
-const cleanCSS = require('gulp-clean-css');
-const uglify = require('gulp-uglify');
-const rename = require('gulp-rename');
+var gulp = require('gulp');  // eslint-disable-line
+var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
-gulp.task('build-script', function () {
-  return gulp.src('lib/magicbook.js')
-      .pipe(uglify())
-      .pipe(rename({extname: '.min.js'}))
-      .pipe(gulp.dest('dist'));
-});
-
-gulp.task('build-css', function () {
+gulp.task('build-css', function buildCss() {
   return gulp.src('public/css/magicbook.css')
       .pipe(cleanCSS({compatibility: 'ie8'}))
       .pipe(rename({extname: '.min.css'}))
       .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build-plugins-markdown-awesome-css', function () {
-  return gulp.src('plugins/markdown/css/awesome.css')
+gulp.task('build-script', function buildScript() {
+  return gulp.src('lib/magicbook.js')
+      .pipe(uglify())
+      .pipe(rename({extname: '.min.js'}))
+      .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build-plugins-markdown-css', function buildPluginsMarkdownCss() {
+  return gulp.src(['plugins/markdown/css/*.css', '!plugins/markdown/css/*.min.css'])
       .pipe(cleanCSS({compatibility: 'ie8'}))
       .pipe(rename({extname: '.min.css'}))
       .pipe(gulp.dest('plugins/markdown/css'));
 });
 
+gulp.task('build-plugins-markdown-script', function buildPluginsMarkdownScript() {
+  return gulp.src(['plugins/markdown/js/*.js', '!plugins/markdown/js/*.min.js'])
+      .pipe(uglify())
+      .pipe(rename({extname: '.min.js'}))
+      .pipe(gulp.dest('plugins/markdown/js'));
+});
+
+gulp.task('build-plugins-components-css', function buildPluginsComponentsCss() {
+  return gulp.src(['plugins/components/css/*.css', '!plugins/components/css/*.min.css'])
+      .pipe(cleanCSS({compatibility: 'ie8'}))
+      .pipe(rename({extname: '.min.css'}))
+      .pipe(gulp.dest('plugins/components/css'));
+});
+
+gulp.task('build-plugins-components-script', function buildPluginsComponentsScript() {
+  return gulp.src(['plugins/components/js/*.js', '!plugins/components/js/*.min.js'])
+      .pipe(uglify())
+      .pipe(rename({extname: '.min.js'}))
+      .pipe(gulp.dest('plugins/components/js'));
+});
+
 gulp.task('default', [
-  'build-script',
   'build-css',
-  'build-plugins-markdown-awesome-css'
+  'build-script',
+  'build-plugins-markdown-css',
+  'build-plugins-markdown-script',
+  'build-plugins-components-css',
+  'build-plugins-components-script'
 ]);
